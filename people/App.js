426e84d0506c43005a8661c92efefd12c1c3f18b
eng-edu <eduardo.eng15@hotmail.com>
2019-05-12 18:26:21 -0300

    up

--- people/App.js
+++ people/App.js
@@ -1,21 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "./src/components/Header";
+import axios from "axios";
 
 export default class App extends React.Component {
+  
+  renderList() {
+    axios
+      .get("https://randomuser.me/api/?nat=br&results=5")
+      .then(res => {
+
+        const  { results } = res.data
+ 
+        const names = results.map(people =>{
+          console.log(people.name.first)
+        })
+
+      })
+      .catch(err => {})
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Header title="Pessoas" />
+        {this.renderList()}
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
