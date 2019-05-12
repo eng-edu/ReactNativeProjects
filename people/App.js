f07605d2795ccf9968c42dd47e4ac301b626a425
eng-edu <eduardo.eng15@hotmail.com>
2019-05-12 19:05:10 -0300

    up

--- people/App.js
+++ people/App.js
@@ -3,21 +3,13 @@ import { StyleSheet, Text, View } from "react-native";
 import Header from "./src/components/Header";
 import axios from "axios";
 
+
 export default class App extends React.Component {
-  
-  renderList() {
-    axios
-      .get("https://randomuser.me/api/?nat=br&results=5")
-      .then(res => {
-
-        const  { results } = res.data
- 
-        const names = results.map(people =>{
-          console.log(people.name.first)
-        })
-
-      })
-      .catch(err => {})
+  constructor(props) {
+    super(props);
+    this.state = {
+      peoples: []
+    };
   }
 
   render() {
@@ -27,5 +19,26 @@ export default class App extends React.Component {
         {this.renderList()}
       </View>
     );
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://randomuser.me/api/?nat=br&results=5")
+      .then(res => {
+        const { results } = res.data;
+        this.setState({
+          peoples: results
+        });
+      })
+      .catch(err => {});
+  }
+
+  renderList() {
+    const textElements = this.state.peoples.map((people) => {
+      const {first} = people.name
+      return <Text key = {first}> {first} </Text>;
+    });
+
+    return textElements;
   }
 }
